ed70bf10d4aecfee8ed640896f5baf8efe6a0afb
Admin <jaipkamat@gmail.com>
2020-07-10 11:29:18 -0700

    Created simple protected route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  return res.json({
+    message: 'This is the home page'
+  })
 });
+
+/* GET some protected route */
+router.get('/protected', (req, res, next) => {
+  return res.json({
+    message: 'This is a protected route!'
+  })
+})
 
 module.exports = router;
